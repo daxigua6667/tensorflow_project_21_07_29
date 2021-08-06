af93f0bc10fad502dbebbb2385da5086a011a2f3
daxigua6667 <52847208+daxigua6667@users.noreply.github.com>
2021-08-06 13:20:07 +0800

    dataset split

--- dataset/main.py
+++ dataset/main.py
@@ -1,29 +1,29 @@
-
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import tensorflow as tf
-print(tf.version.VERSION)
 import tensorflow_datasets as tfds
 import numpy as np
-from tensorflow.keras import  layers, models
+from tensorflow.keras import layers, models
 import matplotlib.pyplot as plt
 from tensorflow import keras
 
+print(tf.version.VERSION)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 show_dataset = tfds.list_builders()
 
 # dataset = tfds.load("coil100", split="train", data_dir="C:/Users/daxigua/tensorflow_datasets/", download=False)
-dataset, dataset_info = tfds.load("coil100", split="train",with_info=True, as_supervised=True)
+dataset, dataset_info = tfds.load("coil100", split="train", with_info=True, as_supervised=True, batch_size=-1)
 # dataset_test = tfds.load("coil100", split="test")
 # dataset_test[:,:] = dataset[0: 30]
 
 
-    # dataset = tfds.load(name="coil100", split="train", data_dir="C:/Users/daxigua/tensorflow_datasets/", download=False)
-assert isinstance(dataset, tf.data.Dataset)
-print(dataset)
 
+# dataset = tfds.load(name="coil100", split="train", data_dir="C:/Users/daxigua/tensorflow_datasets/", download=False)
+# assert isinstance(dataset, tf.data.Dataset)
+print(dataset[1])
 
-
+dataset = tfds.as_numpy(dataset)
+image = dataset[0]
+label = dataset[1]
 # for dataset_example in dataset.take(1):  # 只取一个样本
 #     image, label = dataset_example["image"], dataset_example["label"]
 #
@@ -32,18 +32,16 @@ print(dataset)
 #     print("Label: %d" % label.numpy())
 
 def normalize_img(image, label):
-  """Normalizes images: `uint8` -> `float32`."""
-  return tf.cast(image, tf.float32) / 255., label
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
 
-dataset = dataset.map(
-    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-dataset = dataset.cache()
+
+# dataset = dataset.map(
+#     normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+# dataset = dataset.cache()
 # dataset = dataset.shuffle(dataset_info.splits['train'].num_examples)
-dataset = dataset.batch(128)
-dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
-print(dataset)
-
+# dataset = dataset.batch(128)
+# dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
 # dataset_test_image_normalizaion,  dataset_test_lable_normalizaion= dataset_test["image"]/255.0, dataset["label"]
 
@@ -63,17 +61,16 @@ dataset_train = dataset
 
 info = dataset_builder.info
 print("-----------------------------------------------")
-print(info)
+print(dataset_info)
 print("-----------------------------------------------")
 
 print("-----------------------------------------------")
-print("features", info.features)
+print("features", dataset_info.features)
 print("-----------------------------------------------")
-print("num_classes", info.features["label"].num_classes)
+# print("num_classes", dataset_info.features["label"].num_classes)
 print("-----------------------------------------------")
-print("names", info.features["label"].names)
+# print("names", dataset_info.features["label"].names)
 print("-----------------------------------------------")
-
 
 dataset_test = tfds.load("coil100", split="train")
 fig = tfds.show_examples(info, dataset_test)
@@ -94,10 +91,10 @@ model.add(layers.Dense(10))
 model.summary()
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss=tf.keras.losses.,
               metrics=['accuracy'])
 
-model.fit(dataset, epochs=300)
+model.fit(image, label, epochs=300)
 
 # test_loss, test_acc = model.evaluate(train_image, train_label, verbose=2)
 #
